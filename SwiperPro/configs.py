8945ebe2bbfd5f6d96e81f1c8dbc9220512ff895
Easonchar <1009560311@qq.com>
2019-01-17 17:13:58 +0800

    项目初建

---
+++ SwiperPro/configs.py
@@ -0,0 +1,10 @@
+# 云之迅的短信配置
+YZX_SMS_API = "https://open.ucpaas.com/ol/sms/sendsms"
+YZX_SMS_PARAMS = {
+    "sid":"7d946861e6b2d74b2db38a442a19fd38",
+    "token":"8712938e22c34a179b3cea43ff783b68",
+    "appid": "0e6c046983c74d59948cd381e814503d",
+    "templateid":"422930",
+    "param": "",  # 验证码
+    "mobile": "",  # 手机号
+}
